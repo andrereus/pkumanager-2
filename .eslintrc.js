f0cfa3f7e0a06e1ccb762855dbc7786c708e316e
Andre Reus <andrereus@users.noreply.github.com>
2018-12-26 01:14:11 +0100

    ESLint comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,7 @@ module.exports = {
   parser: 'babel-eslint',
   rules: {
     indent: ['error', 2],
-    // "linebreak-style": [
-    //     "error",
-    //     "unix"
-    // ],
+    // "linebreak-style": ["error", "unix"],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'no-unused-vars': 'off'
